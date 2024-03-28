1b1713d068c18373778e1941771aabf65d460afc
Jorge Caro <informatica.altosaludchile@gmail.com>
2024-03-28 15:11:06 -0300

    Sprint commit1

---
+++ app/fs/Files/UserManager.fs.js
@@ -0,0 +1,21 @@
+const fs = require ("fs")
+const crypto = require("crypto")
+
+class UserManager {
+  constructor(){
+    this.path = "./fs/files/user.json"
+    this.init()
+}
+ init(){
+    const exists = fs.existsSync(this.path)
+    if(!exists){
+        const stringData = JSON.stringify([], null, 2)
+        fs.writeFileSync(this.path, stringData)
+        console.log("Archivo Creado")
+    }
+    else {
+      console.log("Archivo ya existe")
+    }
+ }
+}
+const users = new UserManager()
